f1f615d7c486a4443557377981d13f5e76d52486
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-07-12 20:18:18 -0500

    nathan simmons initial commit

---
+++ cohorts/01_july2018/nathan_simmons/profile.js
@@ -0,0 +1,52 @@
+let nathan_simmons = { // 👈 Your_name replace here...
+    first_name: 'Nathan',
+    last_name: 'Simmons',
+    email: 'nathanhbsimmons@gmail.com',
+    github: 'https://github.com/nathanhbsimmons', // acount url
+    github_username: 'nathanhbsimmons',
+    linkedIn: 'https://www.linkedin.com/in/nathan-simmons-114b5695/',
+    portfolio: 'http://nathanhbsimmons.com/', // portfolio url
+    projects: [
+      {
+        name: 'ACA-Job-Tracker'
+        link: 'https://aca-alumni.github.io/ACA-Job-Tracker' // project url
+        github: 'https://github.com/ACA-Alumni/ACA-Job-Tracker' // github repository url
+        summary: '...loads of fun!' // 2-3 sentences max.
+        tools: [
+          'html',
+          'css',
+          '...'
+        ],
+      },
+      {
+        name: 'Austin Currents'
+        link: 'http://austincurrents.com/' // project url
+        github: 'https://github.com/nathanhbsimmons/finalproject/tree/master/swimmingholeapp' // github repository url
+        summary: "Check your favorite natural water swimming hole, see it's real time water conditions and discover if conditions are optimal for swimming today!!" // 2-3 sentences max.
+        tools: [
+          'React',
+          'Redux',
+          'Material-UI',
+          'fetch'
+        ],
+      }
+      // add more objects as needed.
+    ],
+    other_contact_info: {
+      codePen: '', // acount url ⚠️ Not required
+      twitter: '', // username/handle. ⚠️ Not required
+    },
+    checkpoint_status: {
+      resume: null
+      project1: null
+      project2: null
+      portfolio: null
+      hosting: null
+      linkedIn: null
+      Algorithms: null
+      pair_programming: null
+      job: null
+    }
+  
+  }
+  
