d515a62000585531383c1e3573e82b04d4831a71
TODD FREDERKING <toddfrederking@gmail.com>
2018-07-12 20:27:56 -0500

    ready or not here I come

---
+++ cohorts/01_july2018/toddfrederking/profile.js
@@ -0,0 +1,38 @@
+let alumni_template = { // 👈 Your_name replace here...
+  first_name: 'Todd',
+  last_name: 'Frederking',
+  email: 'toddfrederking@gmail.com',
+  github: 'https://github.com/toddfrederking', // acount url
+  github_username: toddfrederking,
+  linkedIn: 'https://www.linkedin.com/in/toddfrederking/',
+  portfolio: 'https://toddfrederking.github.io/portfolio/', // portfolio url
+  projects: [
+    {
+      name: 'ACA-Job-Tracker'
+      link: 'https://aca-alumni.github.io/ACA-Job-Tracker' // project url
+      github: 'https://github.com/ACA-Alumni/ACA-Job-Tracker' // github repository url
+      summary: '...loads of fun!' // 2-3 sentences max.
+      tools: [
+        'html',
+        'css',
+        'JavaScript'
+      ],
+    },
+  ],
+  other_contact_info: {
+    codePen: 'https://codepen.io/toddfrederking/', // acount url ⚠️ Not required
+    twitter: '@toddfrederking', // username/handle. ⚠️ Not required
+  },
+  checkpoint_status: {
+    resume: 'https://toddfrederking.github.io/web-workbook/01week/resume/index.html'
+    project1: null
+    project2: null
+    portfolio: 'https://toddfrederking.github.io/portfolio/'
+    hosting: null
+    linkedIn: 'https://www.linkedin.com/in/toddfrederking/'
+    Algorithms: null
+    pair_programming: null
+    job: null
+  }
+
+}
