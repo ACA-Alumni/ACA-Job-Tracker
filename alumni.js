d1e16c7c02669e56d19063fa1ef249e7b9e1c8f5
Eric Walz <walzer85@gmail.com>
2018-07-12 20:21:14 -0500

    filled out profile.js

--- alumni.js
+++ alumni.js
@@ -1,14 +1,14 @@
 var _alumni = [
   {
-    class_name: '00_may2018'
+    class_name: '00_may2018',
     alumni: [
       'brance_oats'
     ]
   },
   {
-    class_name: '01_july2018'
+    class_name: '01_july2018',
     alumni: [
-      ''
+      'Eric Walz',
     ]
   }
 
