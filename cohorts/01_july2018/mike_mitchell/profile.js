dd5dd7b89b2c3aba8092402b2870c85a729728ed
mimitch2 <mike@melodix.com>
2018-07-12 20:17:58 -0500

    First push for Mike Mitchell

---
+++ cohorts/01_july2018/mike_mitchell/profile.js
@@ -0,0 +1,53 @@
+let alumni_template = { // 👈 mike_mitchell
+  first_name: 'Mike',
+  last_name: 'Mitchell',
+  email: 'mimitch@mac.com',
+  github: 'https://github.com/mimitch2', // acount url
+  github_username: 'mimitch2',
+  linkedIn: 'https://www.linkedin.com/in/mike-mitchell-706a9117/',
+  portfolio: '', // portfolio url
+  projects: [
+    {
+      name: 'ACA-Job-Tracker'
+      link: 'https://aca-alumni.github.io/ACA-Job-Tracker' // project url
+      github: 'https://github.com/ACA-Alumni/ACA-Job-Tracker' // github repository url
+      summary: '...loads of fun!' // 2-3 sentences max.
+      tools: [
+        'html',
+        'css',
+        '...'
+      ],
+    },
+    {
+      name: 'Cocktail App'
+      link: 'https://mjm-cocktail-app.herokuapp.com/' // project url
+      github: 'https://github.com/mimitch2/mjm-cocktails' // github repository url
+      summary: 'An app that pulls cocktail recipes and images based on type, name or random requests' // 2-3 sentences max.
+      tools: [
+        'React',
+        'Redux',
+        'Express',
+        'Mongoose',
+        'Material-Ui'
+      ],
+    }
+    // add more objects as needed.
+  ],
+  other_contact_info: {
+    codePen: '', // acount url ⚠️ Not required
+    twitter: '', // username/handle. ⚠️ Not required
+  },
+  checkpoint_status: {
+    resume: null
+    project1: null
+    project2: null
+    portfolio: null
+    hosting: null
+    linkedIn: null
+    Algorithms: null
+    pair_programming: null
+    job: null
+  }
+
+}
+
